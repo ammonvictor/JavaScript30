0a5621f9c0bd9a666c7bc0d5caaa61bb485d8fc9
Macbook <macbook@Macbooks-MacBook.local>
2018-06-28 20:35:58 +0300

    Completed Dev tools tricks tut

--- 09-dev-tools-tricks/app.js
+++ 09-dev-tools-tricks/app.js
@@ -7,25 +7,51 @@ function makeGreen() {
 }
 
 // Regular
+console.log('Hello')
 
 // Interpolated
+console.log('I am an %s, I\'m a %s %s, I\'m a %s in %s', '👽', '👨🏽‍⚖️', '👽', '🇯🇲', '🗽')
 
 // Styled
+console.log('%c Styled output', 'background-color:limegreen; color:white;')
 
 // warning!
+console.warn('Oh boy!')
 
 // Error :|
+console.error('Tings are getting gagna gagna!')
 
 // Info
+console.info('Pistachio nut is actually a fruit %s', '😳')
 
 // Testing
+const para = document.querySelector('p')
+console.assert(para.classList.contains('Kame Ha Me Ha'), 'And! you call yourself a ninja')
 
 // clearing
+// console.clear()
 
 // Viewing DOM Elements
+// console.log(para)
+// console.dir(para)
 
 // Grouping together
+dogs.forEach(dog => {
+    console.groupCollapsed(`${dog.name}`)
+    console.log(`${dog.name} is a 🐕 `)
+    console.log(`${dog.name} is ${dog.age} years old`)
+    console.log(`${dog.name} is ${dog.age * 7} years old in human years`)
+    console.groupEnd(`${dog.name}`)
+})
 
 // counting
+console.count(para)
 
 // timing
+console.time('Fetching Data')
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => {
+        console.timeEnd('Fetching Data')
+        console.table(data)
+    })

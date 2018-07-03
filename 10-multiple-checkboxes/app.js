f757fe9919d01bc121ccd24a01335a3fb81eb5cb
Ammon Victor <ammon.victor@gmai.com>
2018-07-03 14:54:36 +0300

    Completed Multiple Checkboxes Tutorial

--- 10-multiple-checkboxes/app.js
+++ 10-multiple-checkboxes/app.js
@@ -1,5 +1,34 @@
 let initialize = () => {
-    // 
+    const inbox = document.querySelector('.inbox')
+    const checkboxes = inbox.querySelectorAll('input[type=checkbox]')
+
+    let lastChecked
+
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('click', handleChecked)
+    })
+    
+    function handleChecked (event) {
+        let inBetween = false
+
+        if (event.shiftKey && this.checked) {
+            checkboxes.forEach(checkbox => {
+
+                console.log(checkbox)
+                
+                if (checkbox === this || checkbox ===  lastChecked) {
+                    inBetween = !inBetween
+                    console.log('In between items')
+                }
+                
+                if (inBetween) {
+                    checkbox.checked = true
+                }
+            })
+        }
+        
+        lastChecked = this
+    }
 }
 
 window.addEventListener('load', initialize)

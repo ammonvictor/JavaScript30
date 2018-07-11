c57b1b7d4ec1b7a7f5ec740ebfc192a645394431
Ammon Victor <ammon.victor@gmai.com>
2018-07-11 10:48:15 +0300

    Added: - Clear all on ui - Hooked up functionality to clear all and individual items - Check to render default message if items is empty

--- 15-localStorage/app.js
+++ 15-localStorage/app.js
@@ -1,6 +1,7 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = JSON.parse(localStorage.getItem('items')) || []
+const clearTapas = document.querySelector('.clear-all-tapas')
 
 function addItem(event) {
 	event.preventDefault()
@@ -17,32 +18,53 @@ function addItem(event) {
 }
 
 function populateList(plates = [], plateslist) {
+	if(!plates.length){
+		// console.error('No items in array')
+		plateslist.innerHTML = "Loading Tapas..."
+		return
+	}
 	plateslist.innerHTML = plates.map((plate, index) => {
 		return `
 			<li>
 				<input type="checkbox" name="" id="item${index}" data-index=${index} ${plate.done ? 'checked': ''}/>
 				<label for="item${index}">${plate.text}</label>
+				<button class="delete" data-index=${index}>➖</button>
 			</li>`;
 	}).join('')
 }
 
 function toggleDone(event) {
-	if (!event.target.matches('input')) return;
+	if (!event.target.matches('input') && !event.target.matches('button')) return;
+
 	const el = event.target
 	const index = el.dataset.index
+	
+	if (el.matches('button')) {
+		items.splice(index, 1)
+	}
 
-	items.map((item, indx) => {
-		if (indx == index) {
-			item.done = !item.done
-		}
-	})
+	if (el.matches('input')) {
+		items.map((item, indx) => {
+			if (indx == index) {
+				item.done = !item.done
+			}
+		})
+	}
+
 	console.log(items)
+	populateList(items, itemsList)
+	localStorage.setItem('items', JSON.stringify(items))
+}
 
+function clearAllTapas() {
+	items.length = 0
+	populateList(items, itemsList)
 	localStorage.setItem('items', JSON.stringify(items))
 }
 
 addItems.addEventListener('submit', addItem)
 itemsList.addEventListener('click', toggleDone)
+clearTapas.addEventListener('click', clearAllTapas)
 
 if (!!items.length) {
 	populateList(items, itemsList)

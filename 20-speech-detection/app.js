7306b051fdb268ec6d1c17ed2617c355d02d342e
Ammon Victor <ammon.victor@gmai.com>
2018-07-12 13:04:28 +0300

    Complete files: Speech Recognition

--- 20-speech-detection/app.js
+++ 20-speech-detection/app.js
@@ -1,1 +1,27 @@
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
+const recognition = new SpeechRecognition()
+
+recognition.interimResults = true
+
+let p = document.createElement('p')
+const words = document.querySelector('.words')
+words.appendChild(p)
+
+function processResults(event) {
+    const transcript = Array.from(event.results).map(result => result[0]).map(result => result.transcript).join()
+    
+    p.textContent = transcript
+    if(event.results[0].isFinal){
+        p = document.createElement('p')
+        words.appendChild(p)
+    }
+    if (transcript.includes('denzel')) {
+        console.log('Equalizer 2')
+    }
+}
+
+recognition.addEventListener('result', processResults)
+recognition.addEventListener('end', recognition.start)
+
+recognition.start()

7392abf2828889f69c9514be2ee4d12b0e80e96b
Ammon Victor <ammon.victor@gmail.com>
2018-07-13 15:21:38 +0300

    Complete: Follow Along Nav

--- 22-follow-along-nav/app.js
+++ 22-follow-along-nav/app.js
@@ -1,2 +1,21 @@
 // 👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
 
+const triggers = Array.from(document.querySelectorAll('a'))
+const highlight = document.createElement('span')
+highlight.classList.add('highlight')
+document.body.append(highlight)
+
+function highlightLink(event) {
+	const link = this.getBoundingClientRect()
+	const coords = {
+		width: link.width,
+		height: link.height,
+		top: link.top + window.scrollY,
+		left: link.left + window.scrollX
+	}
+	// highlight.style = `width:${link.width}px; height:${link.height}px; top:${link.top}px; bottom:${link.bottom}px; left:${link.left}px; right:${link.right}px`
+	highlight.style = `width:${coords.width}px; height:${coords.height}px`
+	highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', highlightLink))
